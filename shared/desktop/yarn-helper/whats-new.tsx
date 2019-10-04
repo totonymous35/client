5e72b85f81d14ee2adeea64402913c7921287bbc
Jacob Young <jacob@jry.io>
2019-10-04 17:10:49 -0400

    Working script

--- shared/desktop/yarn-helper/whats-new.tsx
+++ shared/desktop/yarn-helper/whats-new.tsx
@@ -1,40 +1,22 @@
 /* eslint-disable sort-keys */
 import fs from 'fs'
 import path from 'path'
+import prettier from 'prettier'
 import readline from 'readline'
-import releasesJSON from '../../whats-new/releases-gen.json'
+import rimraf from 'rimraf'
+import releasesJSON from '../../whats-new/releases-gen'
 
 const commands = {
-  'create-release': {
-    code: createRelease,
+  'whats-new': {
+    code: updateWhatsNew,
     help: 'Update release information for "What\'s New" notifier',
   },
 }
-/*
- * Questions
- *
- * 1. *required <string> What is the version of the new release?
- * 2. *required <number> How many new features are being hihglighted?
- * 3. LOOP for {n} features
- *    1. *required <string> "What is the feature text?"
- *    2. *optional <boolean> "Is there an image to include in this feature? (y/n)"
- *        1. *required <string> "What is the path of the image?" (must be in client/shared/images)
- *          - Test that the path is valid and an image exists there
- *          - (?) Perhaps enforce that images are `.png` as well?
- *    3. *optional* <boolean> "Is there a primary button to include in this feature? (y/n)"
- *        1. *required <string> "What is the button text?"
- *        2. *required <string> "What path should the button open?"
- *        3. *required <boolean> "Is this path external (web URL)? (y/n)"
- *    4. *optional <boolean> [if primary = true] "Is there a secondary button to include in this feature? (y/n)"
- *        1. *required <string> "What is the button text?"
- *        2. *required <string> "What path should the button open?"
- *        3. *required <boolean> "Is this path external (web URL)? (y/n)"
- *
- * 4. Confirm that the *oldest* (lastLast) version will be removed from the
- * JSON file and its assets located in `client/shared/iamges/releases` will be
- * deleted
- *
- */
+
+const paths = {
+  releaseGen: path.resolve(__dirname, '..', '..', 'whats-new', 'releases-gen.tsx'),
+  whatsNew: path.resolve(__dirname, '..', '..', './whats-new/'),
+}
 
 type Questions = {
   [key: string]: {
@@ -69,10 +51,9 @@ type Feature = {
 }
 
 // Source: https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-const semverRegex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+const semverRegex = /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/
+const validPathRegex = new RegExp(`^\.\./images/releases/${semverRegex.source}/.*$`)
 const formatQuestion = (question: string) => `❓ ${question}:\n`
-const relativeWhatsNew = './whats-new/'
-const validPathRegex = new RegExp(`^\.\.\/images\/releases\/${semverRegex.source}/.*$`)
 
 const questions: Questions = {
   version: {
@@ -141,19 +122,13 @@ const questions: Questions = {
       required: false,
       text: 'Enter a path to an image asset.\nE.g. ./images/releases/{MAJOR.MINOR.PATCH}/image-name.png',
       validator: answer => {
-        const pathRelativeToWhatsNew = path.relative(relativeWhatsNew, answer)
-        const exists = fs.existsSync(pathRelativeToWhatsNew)
-        const isValidPath = pathRelativeToWhatsNew.match(validPathRegex)
-        console.log('DEBUG: featureImage', {
-          answer,
-          pathRelativeToWhatsNew,
-          exists,
-          isValidPath,
-          validPathRegex: validPathRegex.source,
-        })
+        const relativePathFromWhatsNew = path.relative(paths.whatsNew, answer)
+        const absPathFromWhatsNew = path.resolve(paths.whatsNew, relativePathFromWhatsNew)
+        const isValidPath = relativePathFromWhatsNew.match(validPathRegex)
+        const exists = fs.existsSync(absPathFromWhatsNew)
         return isValidPath && exists
           ? {valid: true, message: ''}
-          : {valid: false, message: `Image file at path ${path.resolve(answer)} does not exist.`}
+          : {valid: false, message: `Image file at path ${absPathFromWhatsNew} does not exist.`}
       },
       update: (currentFeature, answer) => {
         return {
@@ -438,7 +413,7 @@ const chainQuestions = async (rl: readline.Interface, question: Question, next: 
 }
 
 /*
- * createRelease will execute a chain of linked questions that take input via
+ * updateWhatsNew will execute a chain of linked questions that take input via
  * STDIN and build a new release JSON file located at
  * 'client/shared/whats-new/releases-gen.tsx'
  *
@@ -446,7 +421,7 @@ const chainQuestions = async (rl: readline.Interface, question: Question, next: 
  * in the correct location as well as removing old assets when a version is no
  * longer included in 'What's New'
  */
-async function createRelease() {
+async function updateWhatsNew() {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -457,12 +432,6 @@ async function createRelease() {
   const {question: versionQuestion, next: versionNext} = questions.version
   const updatedVersion = await chainQuestions(rl, versionQuestion, versionNext, emptyVersion)
 
-  // -- At this point we have an object that looks like:
-  // {
-  //    version: 'X.X.X',
-  //    features: [ {}, ... ], // with {numFeatures} of empty objects
-  // }
-
   const featuresPromiseChain = updatedVersion.features
     .map((emptyFeature: {}) => {
       const {question: featureTextQuestion, next: featureTextNext} = questions.featureText
@@ -470,64 +439,73 @@ async function createRelease() {
       return args
     })
     // Sequentially execute {numFeatures} chains of feature questions
-    // Then merge each Feature object into an array features = [ Feature1, Feature2, ... ]
+    // Then merge each Feature object into an array features = [ FeatureChain1, FeatureChain2, ... ]
     .reduce(
       (
         prevChain: Promise<Array<Feature>>,
         args: [readline.Interface, Question, nextFn, {}],
         index: number
       ) => {
-        return prevChain.then(features => {
+        return prevChain.then(prevFeatures => {
           console.log('\n')
           console.log(`Feature ${index + 1}`)
           console.log('--------------------')
-          return chainQuestions(...args).then((newFeature: Feature) => {
-            return [...features, newFeature]
+          return chainQuestions(...args).then((nextFeature: Feature) => {
+            return [...prevFeatures, nextFeature]
           })
         })
       },
       Promise.resolve([])
     )
-  const updatedFeatures = await featuresPromiseChain
-  // -- At this pont we have an object that looks like:
-  // {
-  //    version: 'X.X.X',
-  //    features: [
-  //      {
-  //        text: '...',
-  //        image: '...' | null,
-  //        primaryButton: {
-  //          text: '...',
-  //          path: '...',
-  //          external: true | false
-  //        } | null,
-  //        secondaryButton: {
-  //          text: '...',
-  //          path: '...',
-  //          external: true | false
-  //        } | null
-  //      },
-  //
-  //      ...
-  //    ]
-  // }
 
-  const newVersion = {
-    ...updatedVersion,
-    features: updatedFeatures,
-  }
-
-  const newReleaseGen = {
-    current: newVersion,
+  const newFeatures = await featuresPromiseChain
+  const newRelease = {
+    current: {
+      ...updatedVersion,
+      features: newFeatures,
+    },
     last: releasesJSON.current,
     lastLast: releasesJSON.last,
   }
 
-  // Remove images from images directory when releasesJSON.lastLast is no longer used
-  cleanUpRemovedVersion(releasesJSON.lastLast)
+  // Write new release-gen.json
+  try {
+    await writeNewReleaseGen(newRelease)
+  } catch (_) {
+    console.warn(`(Error): Failed to write to ${paths.releaseGen}`)
+    rl.close()
+    return
+  }
 
-  console.log('DEBUG: FINALLY DONE', JSON.stringify(newVersion, null, 4))
+  // Remove images from images directory when releasesJSON.lastLast is no longer used
+  await cleanUpOldImages(rl, releasesJSON.lastLast)
+
   rl.close()
+}
+
+function writeNewReleaseGen(newRelease) {
+  const template = `// NOTE: This file is GENERATED from user input via \`yarn whats-new\`
+//
+// When current.version is different than the user's latest version (synced via
+// gregor) then the items in current.features will be marked as unseen
+/* eslint-disable sort-keys */
+const releases = ${JSON.stringify(newRelease, null, 4)}
+export default releases
+  `
+
+  const formattedRelease = prettier.format(template, {
+    ...prettier.resolveConfig.sync(paths.releaseGen),
+    parser: 'typescript',
+  })
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(paths.releaseGen, formattedRelease, err => {
+      if (err) reject(err)
+      console.log(`✅ Update ${paths.releaseGen}. Final output:`)
+      console.log(formattedRelease)
+      resolve()
+    })
+  })
 }
 
 /*
@@ -536,12 +514,73 @@ async function createRelease() {
  * 3. Tell the user which images exist and ask for each one if they would like to remove them.
  * 4. Delete each file
  */
-function cleanUpRemovedVersion(release) {
-  if (release.features && release.features.length) {
-    const imagePaths = release.features
+async function cleanUpOldImages(rl, oldestRelease) {
+  if (oldestRelease.features && oldestRelease.features.length) {
+    const oldImagePaths: Array<string> = oldestRelease.features
       .map((feature: Feature) => (feature && feature.image ? feature.image : null))
       .filter(Boolean)
-    console.log('DEBUG: cleanUpRemovedVersion', {imagePaths})
+      .filter((p: string) => p.match(validPathRegex))
+
+    if (!oldImagePaths.length) {
+      return
+    }
+
+    const enclosingDirsMap = oldImagePaths.reduce((acc, p) => {
+      const {dir: enclosingDirPath} = path.parse(path.resolve(__dirname, path.relative(paths.whatsNew, p)))
+      const {base} = path.parse(enclosingDirPath)
+      const isSemverDir = base.match(semverRegex) !== null
+      return {
+        ...acc,
+        [enclosingDirPath]: isSemverDir,
+      }
+    }, {})
+
+    // Check if any images do not match `images/oldestReleases/MAJOR.MINOR.PATCH/filename.ext`
+    const nonMatchingImages = Object.values(enclosingDirsMap).filter(d => !d)
+    if (nonMatchingImages.length) {
+      console.warn(
+        'The following images are not located in "images/oldestReleases/{MAJOR.MINOR.PATCH}/", remove them manually',
+        '\n',
+        nonMatchingImages.join('\n')
+      )
+      return
+    }
+
+    const directoryDeleteConfirm =
+      'This will delete the following directories' +
+      '\n\t* ' +
+      Object.keys(enclosingDirsMap).join('\n\t* ') +
+      '\nProceed? (y/n)'
+
+    const answer = await requiredQuestion(rl, directoryDeleteConfirm, input =>
+      input === 'y' || input === 'n'
+        ? {valid: true, message: ''}
+        : {valid: false, message: 'Enter either (y/n)'}
+    )
+
+    if (answer === 'n') {
+      return
+    }
+
+    const enclosingDirsPaths = Object.keys(enclosingDirsMap)
+    const removeDirsPromiseChain = enclosingDirsPaths.reduce((prevChain, dirPath) => {
+      return prevChain.then(() => {
+        return new Promise((resolve, reject) => {
+          rimraf(dirPath, {}, (err: Error) => {
+            if (err) {
+              return reject(dirPath)
+            }
+            resolve()
+          })
+        })
+      })
+    }, Promise.resolve())
+
+    try {
+      await removeDirsPromiseChain
+    } catch (err) {
+      console.warn(`(Error): Failed to remove directory ${err}`)
+    }
   }
 }
 
