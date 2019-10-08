b5301067deabace50cdf8f94728e703d3df0fbf1
Jacob Young <jacob@jry.io>
2019-10-08 15:03:47 -0400

    WIP

--- shared/whats-new/container.tsx
+++ shared/whats-new/container.tsx
@@ -3,16 +3,6 @@ import * as Container from '../util/container'
 import WhatsNew from '.'
 // import {currentVersion, lastVersion, lastLastVersion} from './releases'
 
-/*
- * TODO Developer Notes
- *
- * 1. Make an RPC (ON OPEN) to gregor to get the user's last seen version string
- * 2. Make an RPC (ON CLOSE) to gregor to set the user's last seen version to the current one
- * 3. Read the releses JSON information
- * 4. Combining the latest version with the JSON information, set the "seen state"
- *
- */
-
 // TODO @jacob: Need to subscribe to the state of "seen" versions
 // Probably need to make a reducer
 const mapStateToProps = () => ({})
@@ -31,4 +21,4 @@ const mergeProps = (stateProps, dispatchProps) => ({
 
 const WhatsNewContainer = Container.connect(mapStateToProps, mapDispatchToProps, mergeProps)(WhatsNew)
 
-export default Container
+export default WhatsNewContainer
--- shared/whats-new/header-icon/container.tsx
+++ shared/whats-new/header-icon/container.tsx
@@ -1,6 +1,21 @@
-import HeaderIcon, {HeaderIconWithPopup} from './index'
+import * as Container from '../../util/container'
+import HeaderIconComponent, {HeaderIconWithPopup as HeaderIconWithPopupComponent} from './index'
 
 // TODO @jacob: Check if there is a new release here and set the icon state & color
 
-export default HeaderIcon
+const mapStateToProps = (state: Container.TypedState) => ({})
+
+const mapDispatchToProps = (_: Container.TypedDispatch) => ({})
+
+const mergeProps = (stateProps, dispatchProps) => ({})
+
+const HeaderIconContainer = Container.connect(mapStateToProps, mapDispatchToProps, mergeProps)(
+  HeaderIconComponent
+)
+
+const HeaderIconWithPopup = Container.connect(mapStateToProps, mapDispatchToProps, mergeProps)(
+  HeaderIconWithPopupComponent
+)
+
+export default HeaderIconContainer
 export {HeaderIconWithPopup}
